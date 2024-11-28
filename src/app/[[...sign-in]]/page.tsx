8938255b66e60a0f5bcb57f37a23094b4aa964c6
caioneves05 <ncaio037@gmail.com>
2024-11-27 23:55:26 -0300

    feat: initial login page

---
+++ src/app/[[...sign-in]]/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import { LoginForm } from "@/components/LoginForm";
+
+const LoginPage = () => {
+    return(
+        <div className="h-screen flex items-center justify-center bg-lamaSkyLight">
+            <div className="bg-white h-[75%] w-[30%] rounded-tl-xl rounded-bl-xl" /> 
+            <div className="bg-orange-400 h-[75%] w-[30%] rounded-tr-xl rounded-br-xl" /> 
+        </div>
+    )
+}
+
+export default LoginPage;
